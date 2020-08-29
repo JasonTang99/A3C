e721ab4deb052ceab375b84e3d1f7c7dce613a49
JasonTang99 <1999jasontang@gmail.com>
2020-08-29 16:05:08 -0400

    Added gif generation

--- torch_a3c.py
+++ torch_a3c.py
@@ -45,12 +45,16 @@ class SharedRMSprop(optim.RMSprop):
                 state['square_avg'] = torch.zeros_like(p.data)
                 state['momentum_buffer'] = torch.zeros_like(p.data)
                 state['grad_avg'] = torch.zeros_like(p.data)
-                
+
                 state['square_avg'].share_memory_()
                 state['momentum_buffer'].share_memory_()
                 state['grad_avg'].share_memory_()
 
+
 def transform(image_in_1, image_in_2):
+    # Performs preproocessing of atari environment as described in:
+    # Human-level control through deep reinforcement learning by Mnih et al.
+
     # Take max pixel values between the 2 frames
     image_in = np.maximum(image_in_1, image_in_2)
     # To PIL image
@@ -62,7 +66,8 @@ def transform(image_in_1, image_in_2):
     # Back to Tensor
     return transforms.functional.to_tensor(image_out)
 
-def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_reward):
+def train(rank, args, device, global_model, opt, opt_lock, 
+          step_counter, ma_reward, ma_loss):
     torch.manual_seed(args.seed + rank)
 
     # Setup env
@@ -76,7 +81,7 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
     # Keep a deque of 4 processed states as input
     proc_state = transform(state, next_state)
     proc_states = deque([
-        proc_state.clone(), proc_state.clone(), 
+        proc_state.clone(), proc_state.clone(),
         proc_state.clone(), proc_state.clone()
     ], maxlen=4)
     state = next_state
@@ -90,7 +95,7 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
     while step_counter.value < args.max_steps:
         # Sync Models
         local_model.load_state_dict(global_model.state_dict())
-        
+
         # Run simulation until episode done or update time
         values, rewards, actions, action_probs = [], [], [], []
         t_start = thread_step_counter
@@ -103,7 +108,7 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
             probs = F.softmax(logits, dim=-1)
             action = torch.multinomial(probs, 1).detach()
             next_state, reward, done, _ = env.step(action)
-            
+
             # Update states
             proc_state = transform(state, next_state)
             proc_states.append(proc_state)
@@ -114,12 +119,13 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
             values.append(value)
             actions.append(action)
             action_probs.append(probs)
-            
+
             # Update counters
-            step_counter.value  += 1
+            step_counter.value += 1
             thread_step_counter += 1
-        
-        # print("{:.2f}%".format(100 * step_counter.value / args.max_steps))
+
+        # if step_counter.value % 100 == 0:
+        #     print("{:.2f}%".format(100 * step_counter.value / args.max_steps))
         # env.render()
 
         rewards = torch.tensor(rewards).unsqueeze(1)  # (t, 1)
@@ -133,14 +139,14 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
             processed_input = torch.cat(tuple(proc_states)).unsqueeze(0)
             _, value = local_model(processed_input)
             R = value.detach()
-        
+
         # Compute Returns
         returns = []
         for i in range(len(rewards))[::-1]:
             R = rewards[i] + args.gamma * R
             returns.extend(R)
-        returns = torch.stack(returns[::-1]) # (t, 1)
-        
+        returns = torch.stack(returns[::-1])  # (t, 1)
+
         # Compute values needed in loss calculation
         t = action_probs.shape[0]
         log_probs = torch.log(action_probs)                     # (t, action_space)
@@ -149,29 +155,24 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
         entropy = -torch.sum(action_probs * log_probs) / t      # (1)
 
         # Calculate losses
-        actor_loss = -torch.sum(log_action_probs * advantage) - args.beta * entropy
+        actor_loss = -torch.sum(log_action_probs *
+                                advantage) - args.beta * entropy
         critic_loss = torch.sum(advantage.pow(2))
         total_loss = actor_loss + critic_loss
-
-        # print(entropy.item(), -torch.sum(log_action_probs * advantage).item())
-        # print("Advantage", advantage)
-        # print("Actions", actions)
-        # print("Probs", action_probs)
-        # print("Log Probs", log_action_probs)
-        # print("Loss", actor_loss.item(), critic_loss.item())
 
         # Calculate gradient and clip to maximum norm
         total_loss.backward()
         nn.utils.clip_grad_norm_(local_model.parameters(), args.max_grad)
-        
+
         # Propogate gradients to shared model
         with opt_lock:
-            for l_param, g_param in zip(local_model.parameters(), global_model.parameters()):
+            for l_param, g_param in zip(local_model.parameters(), 
+                                        global_model.parameters()):
                 g_param._grad = l_param.grad.clone()
             opt.step()
             opt.zero_grad()
         local_model.zero_grad()
-        
+
         with torch.no_grad():
             ep_reward += torch.sum(rewards).item()
             ep_loss += total_loss.item()
@@ -179,11 +180,14 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
             with torch.no_grad():
                 if ma_reward.value == 0.0:
                     ma_reward.value = ep_reward
+                    ma_loss.value = ep_loss
                 else:
                     ma_reward.value = ma_reward.value * 0.95 + ep_reward * 0.05
+                    ma_loss.value = ma_loss.value * 0.95 + ep_loss * 0.05
 
-                print(f"Moving Average Reward: {ma_reward.value:.2f}\t" +
-                      f"Episode Reward: {ep_reward}\tLoss: {ep_loss:.4E}\t" +
+                print(f"MA Reward: {ma_reward.value:.2f}\t" +
+                      f"MA Loss: {ma_loss.value:.2f}\t" +
+                      f"EP Reward: {ep_reward}  \tEP Loss: {ep_loss:.4E}\t" +
                       f"Thread-{rank} Steps: {thread_step_counter}")
 
                 ep_reward, ep_loss = 0.0, 0.0
@@ -200,6 +204,59 @@ def train(rank, args, device, global_model, opt, opt_lock, step_counter, ma_rewa
                 ], maxlen=4)
                 state = next_state
 
+
+def test(args, device, model, tries=3, max_steps=1000000):
+    torch.manual_seed(args.seed)
+
+    # Setup env
+    env = gym.make('SpaceInvaders-v0')
+    state_size = env.observation_space.shape
+    action_size = env.action_space.n
+
+    # Run the simulation "tries" times
+    for t in range(tries):
+        # Reset Env
+        state = env.reset()
+        next_state, _, _, _ = env.step(0)
+
+        # Keep a deque of 4 processed states as input
+        proc_state = transform(state, next_state)
+        proc_states = deque([
+            proc_state.clone(), proc_state.clone(),
+            proc_state.clone(), proc_state.clone()
+        ], maxlen=4)
+        state = next_state
+
+        # Run simulation until episode done
+        images = []
+        ep_reward = 0.0
+        for i in range(max_steps):
+            if i % 3 == 0:
+                env.render()
+                screen = env.render(mode='rgb_array')
+                images.append(Image.fromarray(screen))
+            
+            # Take action according to policy
+            processed_input = torch.cat(tuple(proc_states)).unsqueeze(0)
+            logits, value = model(processed_input)
+            probs = F.softmax(logits, dim=-1)
+            action = torch.multinomial(probs, 1).detach()
+            next_state, reward, done, _ = env.step(action)
+
+            # Update states
+            proc_state = transform(state, next_state)
+            proc_states.append(proc_state)
+            state = next_state
+
+            ep_reward += reward
+            if done:
+                break
+        
+        # Save as gif
+        print(f'Try #{t} reward: {ep_reward}')
+        images[0].save(f'invaders-{t}.gif', save_all=True,
+                    append_images=images[1:], loop=0, duration=1)
+    
 
 class ActorCritic(nn.Module):
     def __init__(self, state_size, action_size):
@@ -222,18 +279,19 @@ class ActorCritic(nn.Module):
         self.critic = nn.Linear(256, 1)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x)) # (N, 16, 20, 20)
-        x = F.relu(self.conv2(x)) # (N, 32, 9, 9)
+        x = F.relu(self.conv1(x))  # (N, 16, 20, 20)
+        x = F.relu(self.conv2(x))  # (N, 32, 9, 9)
         x = F.relu(self.fc(x.view(x.shape[0], -1)))  # (N, 256)
 
-        a = self.actor(x) # (N, 6)
-        c = self.critic(x) # (N, 1)
+        a = self.actor(x)  # (N, 6)
+        c = self.critic(x)  # (N, 1)
         return a, c
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch A3C')
-    parser.add_argument('--max_steps', type=int, default=100,
-                        help='number of steps to train (default: 100)')
+    parser.add_argument('--max_steps', type=int, default=10000,
+                        help='number of steps to train (default: 10000)')
     parser.add_argument('--lr', type=float, default=0.001,
                         help='learning rate (default: 0.001)')
     parser.add_argument('--alpha', type=float, default=0.99,
@@ -251,16 +309,17 @@ if __name__ == '__main__':
     parser.add_argument('--max_grad', type=float, default=5.0,
                         help='value to clip gradients at (default: 5.0)')
     parser.add_argument('--num-processes', type=int, default=-1,
-                        help='number of training processes (default: -1 = cpu count)')
+                        help='number of training processes (default: Max)')
     parser.add_argument('--seed', type=int, default=1,
                         help='random seed (default: 1)')
     parser.add_argument('--save-fp', type=str, default=None,
                         help='path to save model (default: None)')
     parser.add_argument('--load-fp', type=str, default=None,
                         help='path to load model (default: None)')
+    parser.add_argument('--method', type=str, default="train",
+                        help='train or test (default: train)')
     args = parser.parse_args()
 
-    start = time.time()
     device = torch.device("cpu")
     torch.manual_seed(args.seed)
     mp.set_start_method('spawn')
@@ -270,30 +329,36 @@ if __name__ == '__main__':
     action_size = env.action_space.n
 
     model = ActorCritic(state_size, action_size).to(device)
-    model.share_memory()
-    
     if args.load_fp:
         model.load_state_dict(torch.load(args.load_fp))
-    # model.load_state_dict(torch.load("a3c.pt"))
-    model.train()
 
-    opt = SharedRMSprop(model.parameters(), args.lr, args.alpha, args.weight_decay, args.momentum, False)
-    opt_lock = mp.Lock()
+    if args.method == "train":
+        start = time.time()
 
-    step_counter, ma_reward = mp.Value('d', 0.0), mp.Value('d', 0.0)
-    processes = []
-    if args.num_processes == -1:
-        args.num_processes = mp.cpu_count()
-        # args.max_steps = 10000
-    for rank in range(args.num_processes):
-        p = mp.Process(target=train, args=(
-            rank, args, device, model, opt, opt_lock, step_counter, ma_reward))
-        p.start()
-        processes.append(p)
-    for p in processes:
-        p.join()
+        model.share_memory()
+        model.train()
 
-    print(f"Seconds taken: {time.time() - start}")
-    if args.save_fp:
-        torch.save(model.state_dict(), args.save_fp)
-    # torch.save(model.state_dict(), "a3c.pt")
+        opt = SharedRMSprop(model.parameters(), args.lr, args.alpha,
+                            args.weight_decay, args.momentum, False)
+        opt_lock = mp.Lock()
+
+        step_counter, ma_reward, ma_loss = mp.Value(
+            'd', 0.0), mp.Value('d', 0.0), mp.Value('d', 0.0)
+        processes = []
+        if args.num_processes == -1:
+            args.num_processes = mp.cpu_count()
+        for rank in range(args.num_processes):
+            p = mp.Process(target=train, args=(
+                rank, args, device, model, opt, opt_lock, 
+                step_counter, ma_reward, ma_loss))
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
+
+        print(f"Seconds taken: {time.time() - start}")
+        if args.save_fp:
+            torch.save(model.state_dict(), args.save_fp)
+    else:
+        test(args, device, model)
+
